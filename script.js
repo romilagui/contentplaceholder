7ca69f042e63e4015fe7c56a322239c8ca14afb8
Romila Rangel <85246670+romilagui@users.noreply.github.com>
2022-04-28 16:56:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const date = document.getElementById('date');
 const animated_bgs = document.querySelectorAll('.animated-bg');
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
 
-setTimeout(getData);
+setTimeout(getData, 2500);
 function getData() {
     header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&w=1000&q=80" alt="unsplash laptop"/>';
     title.innerHTML = 'Lorem ipsum dolor sit amet.';
